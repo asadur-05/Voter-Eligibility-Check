d8f21f0b6d7566b18dcb6cd2a9c768079f09e943
Md. Asadur Rahaman <asadurrahaman2005@gmail.com>
2025-04-13 13:01:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function check(){
+    const name = document.querySelector("#name")
+    const mob = document.querySelector("#phone")
+    const ageinput = document.querySelector("#age")
+    const age=parseInt(ageinput.value)
+    const msg =document.querySelector("#msg")
+    msg.textContent="Checking details...";
+
+    setTimeout(() => {
+        if(isNaN(age)){
+            msg.textContent = name.value +  " Enter a valid age!!"
+            msg.style.color = "red";
+            return ;
+        }
+    
+        if(age >=18){
+            msg.textContent = name.value +  " You are eligible for vote"
+            msg.style.color = "green";
+        }
+        else{
+            msg.textContent = name.value +  " You are NOT eligible for vote"
+            msg.style.color = "red";
+        }
+    }, 2500);
+
+}
